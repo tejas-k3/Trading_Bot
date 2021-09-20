6dba5a9fa81975dd9e1fdae6ea03743219336022
Tejas <tejaskothari03@gmail.com>
2021-09-20 21:03:19 +0530

    Adding file for Portfolio Providing Service
    Adding script for Portfolio Information Provider

---
+++ Portfolio_Provider.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+"""
+Input : A list of string values
+O/P : JSON Object
+Portfolio Info consists
+:- Quarterly Report
+:- Net Value
+:- Sector (Seasonal dependencies)
+:- Involved Stock name
+:- Cash Flow
+:- Balance Sheet
+"""
+
+# Start the driver
+with webdriver.Firefox() as driver:
+    # Open URL
+    driver.get("https://seleniumhq.github.io")
+
+    # Setup wait for later
+    wait = WebDriverWait(driver, 10)
+
+    # Store the ID of the original window
+    original_window = driver.current_window_handle
+
+    # Check we don't have other windows open already
+    assert len(driver.window_handles) == 1
+
+    # Click the link which opens in a new window
+    driver.find_element(By.LINK_TEXT, "new window").click()
+
+    # Wait for the new window or tab
+    wait.until(EC.number_of_windows_to_be(2))
+
+    # Loop through until we find a new window handle
+    for window_handle in driver.window_handles:
+        if window_handle != original_window:
+            driver.switch_to.window(window_handle)
+            break
+
+    # Wait for the new tab to finish loading content
+    wait.until(EC.title_is("SeleniumHQ Browser Automation"))
