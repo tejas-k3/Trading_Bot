34edf8b46c5180e6b2b3aec2b3326f2652cb68cd
Tejas <tejaskothari03@gmail.com>
2022-12-19 05:11:33 +0530

    Start of V2

--- CONSTANT.py
+++ CONSTANT.py
@@ -1,14 +1,8 @@
-import os
 # Project CONSTANTS
-versionName = "Dev_V1.0.0.1"
+versionName = "2.0.0"
+processLimit = 8 # i7 11Gen
 
-#Hardware limitation :(
-# processLimit = 3 # For i5 8th Gen
-processLimit = 8 # i7 11Gen at min
-
-
-
-# Configurable variables for tuning the logic to have better confidence
+# System configs
 ROELowerLimit=15
 ROEUpperLimit=20
 ROE_HighlyConfidenceValue = 1.5
@@ -24,15 +18,6 @@ ROCEUpperLimit = 50
 ROCE_HighlyConfidenceValue = 1.5
 ROCE_FairlyConfidenceValue = 0.75
 
-globalFailed=[]
-
-
-"""Links and system variables"""
-# Path to chrome binary
-CHROME_PATH = r'C:/Program Files/Google/Chrome/Application/chrome.exe'
-# Path to chrome driver
-DRIVER_PATH = os.getcwd()+'//ExcecutableDependencies//chromedriver.exe'
+"""Links"""
 # URL to BSE Equity Market
 BSE_URL = "https://www.bseindia.com/eqstreamer/StreamerMarketwatch.html"
-# URL to Screener
-SCREENER_URL = "https://www.screener.in/company/"
--- coreProcess.py
+++ coreProcess.py
@@ -1,20 +1,16 @@
 """
 This is the core file executing project files.
 """
-from libs import DataParser, Portfolio_Provider
-import Company_Filtration
 import INFO
 import CONSTANT
-import time
 from time import perf_counter
 import multiprocessing
-import pywhatkit
 import logging.config
 logging.config.dictConfig({
     'version': 1,
     'disable_existing_loggers': True,
 })
-logging.config.fileConfig(fname='ezMoneyLOGGER.conf')
+logging.config.fileConfig(fname='logger.conf')
 LOGGER = logging.getLogger('root')
 
 segments = INFO.marketSegments
@@ -25,53 +21,5 @@ if __name__ == '__main__':
     # Total companies to do analysis on
     companies = []
     # Set to handle duplications
-    setOfCompanies = set()
-    # Process pool 
-    pool = multiprocessing.Pool(processes = CONSTANT.processLimit) # LIMIT FOR TK
-    # Get company names for given segments
-    results = pool.map(Portfolio_Provider.getCompanies, segments)
-    for result  in results:
-        companies+=result
-    # Handle repetition of company names
-    setOfCompanies.update(val for val in companies if val[0] not in [i[0] for i in setOfCompanies])
-    # Flush old values
-    totalNames = len(setOfCompanies)
-    companies.clear()
-    # Parse information for processing
-    results = pool.map(Portfolio_Provider.parsingMethod, setOfCompanies)
-    for result in results:
-        companies.append(result)
-    # Clear out invalid formats
-    companies = list(filter(None, companies))
-    totalCompaniesToProcess = len(companies)
-    DataParser.jsonFileStore(companyList=companies, name="/logs/"+time.strftime("%Y-%m-%d-%H_%M_%S")+"coreProcess_companies.json", enList = False)
-    # Total probable profitable companies
-    profitableCompanies = []
-    profitableCompanies = Company_Filtration.profitableCompanies(companies)
-    totalProfitCompanies = len(profitableCompanies)
-    for x in profitableCompanies:
-        print(x['Name'], end=', ')
-    DataParser.jsonFileStore(companyList=profitableCompanies, name="/logs/"+time.strftime("%Y-%m-%d-%H_%M_%S")+"coreProcess_profitableCompanies.json", enList = False)
-    # End time in seconds
     endTime = perf_counter()
-    LOGGER.info("Ended main process at {}".format(endTime))
     LOGGER.info("Total time taken for core process to run : {} minutes".format((endTime-startTime)/60))
-    LOGGER.info("{names} names, {pro} processed companies, {selected} selected companies".format(names=totalNames, pro=totalCompaniesToProcess, selected=totalProfitCompanies))
-    messageForWhatsapp = "Validate and invest in these stocks :\n"
-    for kompany in profitableCompanies:
-        messageForWhatsapp += (kompany['Name'])
-        messageForWhatsapp += ", "
-    messageForWhatsapp += "done!\nThis was done by {}".format(CONSTANT.versionName)
-    # pywhatkit.sendwhatmsg_instantly("MobileNumber", messageForWhatsapp, 30, True, 30)
-    statusReport = "{names} names, {pro} processed companies, {selected} selected companies".format(names=totalNames, pro=totalCompaniesToProcess, selected=totalProfitCompanies)
-    # pywhatkit.sendwhatmsg_instantly("MobileNumber", statusReport, 30, True, 30)
-    LOGGER.info("Iteration completed by version {} with process limit {} under .".format(CONSTANT.versionName, CONSTANT.processLimit))
-    #L1 Filtration
-    # filteredCompanies = Company_Filtration.profitableCompanies(DataParser.convertToJSONList(Portfolio_Provider.metadataCompanies()))
-    #L2 Current Stock options
-    # selectedStocks = Stock_Provider.stocksList(filteredCompanies)
-    #L2 Filtration
-    #Seasonality should be considered
-    #Fucking wars should be considered
-    #Timer should be put
-    # !!! Type mismatch may occur
